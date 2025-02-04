f3b80e2cbdbe292c4e3af810beca8ecb48298bac
tomason182 <elotro182@hotmail.com>
2025-02-04 20:29:38 -0300

    add: Tabs component

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,24 @@
 import "./App.css";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
+import Tabs from "./components/Tabs/Tabs";
 
 function App() {
   const propertyName = "La Casa de Tomas";
   const user = {
     name: "Tomas",
   };
+
+  const tabs = [
+    { label: "home", content: <h1>Home page</h1> },
+    { label: "calendar", content: <h1>Calendar page</h1> },
+  ];
   return (
     <>
       <Header user={user} propertyName={propertyName} />
-      <main>This is the body</main>
+      <main>
+        <Tabs tabs={tabs} />
+      </main>
       <footer>
         <Footer />
       </footer>
---
+++ src/components/Tabs/Tabs.jsx
@@ -0,0 +1,44 @@
+import styles from "./Tabs.module.css";
+import { useState } from "react";
+import PropTypes from "prop-types";
+
+export default function Tabs({ tabs, defaultActiveTab = 0, onTabChange }) {
+  const [activeTab, setActiveTab] = useState(defaultActiveTab);
+
+  function handleTabClick(index) {
+    setActiveTab(index);
+    if (onTabChange) {
+      onTabChange(index);
+    }
+  }
+
+  return (
+    <div className={styles.tabsWrapper}>
+      <div className={styles.tabsHeader}>
+        {tabs.map((tab, index) => (
+          <button
+            key={index}
+            className={`${styles.tabButton} ${
+              index === activeTab ? styles.activeTabBtn : ""
+            }`}
+            onClick={() => handleTabClick(index)}
+          >
+            {tab.label}
+          </button>
+        ))}
+      </div>
+      <div className={styles.tabContent}>{tabs[activeTab].content}</div>
+    </div>
+  );
+}
+
+Tabs.propTypes = {
+  tabs: PropTypes.arrayOf(
+    PropTypes.shape({
+      label: PropTypes.string.isRequired,
+      content: PropTypes.node.isRequired,
+    })
+  ),
+  defaultActiveTab: PropTypes.number.isRequired,
+  onTabChange: PropTypes.func.isRequired,
+};
