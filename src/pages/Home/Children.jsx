fe02edc9638a11b204faa490b3f94e5c33807821
tomason182 <elotro182@hotmail.com>
2025-02-05 19:00:14 -0300

    add: today's reservations

---
+++ src/pages/Home/Children.jsx
@@ -0,0 +1,79 @@
+import Spinner from "../../components/Spinner/Spinner";
+import { format } from "date-fns";
+import PropTypes from "prop-types";
+import styles from "./Children.module.css";
+
+export function ReservationsList({ data, error, loading, info }) {
+  if (loading) {
+    return <Spinner />;
+  }
+  if (error) {
+    return <h1>Error</h1>;
+  }
+
+  const today = format(new Date(), "yyyyMMdd");
+
+  let filteredData;
+
+  if (info.type === "coming") {
+    filteredData = data.filter(d => {
+      const checkIn = d.check_in.split("-").join("");
+      return (
+        checkIn === today &&
+        (d.status === "confirmed" || d.status === "provisional")
+      );
+    });
+  } else {
+    filteredData = data.filter(d => {
+      const checkOut = d.check_out.split("-").join("");
+      return (
+        checkOut === today &&
+        (d.status === "confirmed" || d.status === "provisional")
+      );
+    });
+  }
+
+  const reservationsList =
+    filteredData.length === 0 ? (
+      <li>{info.message}</li>
+    ) : (
+      filteredData.map(d => (
+        <li key={d.id}>
+          <p>{d.guest_info.full_name}</p>
+          <span>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="#000000"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            >
+              <rect x="3" y="4" width="18" height="18" rx="2" ry="2"></rect>
+              <line x1="16" y1="2" x2="16" y2="6"></line>
+              <line x1="8" y1="2" x2="8" y2="6"></line>
+              <line x1="3" y1="10" x2="21" y2="10"></line>
+            </svg>
+            {d.check_in} | {d.check_out}
+          </span>
+          <span>{d.number_of_guest} pers</span>
+        </li>
+      ))
+    );
+
+  return <ul className={styles.list}>{reservationsList}</ul>;
+}
+
+export function LatestReservations() {
+  return <h1>Return the last 10 reservations</h1>;
+}
+
+ReservationsList.propTypes = {
+  data: PropTypes.array.isRequired,
+  error: PropTypes.string.isRequired,
+  loading: PropTypes.bool.isRequired,
+  info: PropTypes.object.isRequired,
+};
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,39 +1,137 @@
-import Card from "../../components/Card";
+import { useState, useEffect } from "react";
+import Card from "../../components/Card/Card";
+import { ReservationsList, LatestReservations } from "./Children";
 import styles from "./Home.module.css";
 
 export default function Home() {
-  function handleClick() {
-    console.log("Button Clicked");
-  }
+  const [todaysReservations, setTodaysReservations] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const reservations = [
+      {
+        id: 1,
+        guest_info: {
+          full_name: "Larry Clark",
+        },
+        check_in: "2025-02-05",
+        check_out: "2025-02-07",
+        number_of_guest: 2,
+        status: "confirmed",
+      },
+      {
+        id: 2,
+        guest_info: {
+          full_name: "Sophia Martinez",
+        },
+        check_in: "2025-02-05",
+        check_out: "2025-02-09",
+        number_of_guest: 1,
+        status: "confirmed",
+      },
+      {
+        id: 3,
+        guest_info: {
+          full_name: "Michael Johnson",
+        },
+        check_in: "2025-02-05",
+        check_out: "2025-02-07",
+        number_of_guest: 3,
+        status: "confirmed",
+      },
+      {
+        id: 4,
+        guest_info: {
+          full_name: "Emma Thompson",
+        },
+        check_in: "2025-02-05",
+        check_out: "2025-02-12",
+        number_of_guest: 2,
+        status: "confirmed",
+      },
+      {
+        id: 5,
+        guest_info: {
+          full_name: "Daniel Rivera",
+        },
+        check_in: "2025-02-05",
+        check_out: "2025-02-06",
+        number_of_guest: 1,
+        status: "confirmed",
+      },
+    ];
+
+    setTodaysReservations(reservations);
+  }, []);
 
   const actions = [
     {
       label: "SHOW MORE",
-      onClick: () => handleClick(),
-    },
-    {
-      label: "EDIT",
-      onClick: () => handleClick(),
+      onClick: () => console.log("button clicked"),
     },
   ];
 
-  const children = (
-    <ul>
-      <li>First item</li>
-      <li>Second item</li>
-    </ul>
-  );
+  const loadingStyling = {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    padding: "2rem 0",
+  };
+
+  const listStyle = {
+    maxHeight: "300px",
+    overflowY: "auto",
+    overflowX: "hidden",
+  };
+
+  const coming = {
+    type: "coming",
+    message: "No reservations coming today",
+  };
+
+  const leaving = {
+    type: "leaving",
+    message: "No reservations leaving today",
+  };
+
+  const latest = {
+    type: "latest",
+    message: "No reservations in the last 30 days",
+  };
 
   return (
     <div className={styles.container}>
-      <Card title="Who's coming today" actions={actions}>
-        {children}
+      <Card
+        title="Who's coming today"
+        actions={actions}
+        customStyle={loading ? loadingStyling : listStyle}
+      >
+        <ReservationsList
+          data={todaysReservations}
+          error={error}
+          loading={loading}
+          info={coming}
+        />
       </Card>
-      <Card title="Who's leaving today" actions={actions}>
-        {children}
+      <Card
+        title="Who's leaving today"
+        actions={actions}
+        customStyle={loading ? loadingStyling : {}}
+      >
+        <ReservationsList
+          data={todaysReservations}
+          error={error}
+          loading={loading}
+          info={leaving}
+        />
       </Card>
-      <Card title="Latest reservations" actions={actions}>
-        {children}
+      <Card
+        title="Latest reservations"
+        actions={actions}
+        customStyle={loading ? loadingStyling : {}}
+      >
+        <LatestReservations />
       </Card>
     </div>
   );
