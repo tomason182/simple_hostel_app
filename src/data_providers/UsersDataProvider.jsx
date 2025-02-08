f40c9614763df4e2f589ea00debe4fa677dcffdb
tomason182 <elotro182@hotmail.com>
2025-02-08 16:41:21 -0300

    add: all property users data provider

---
+++ src/data_providers/UsersDataProvider.jsx
@@ -0,0 +1,44 @@
+import { useCallback, useEffect, useState } from "react";
+
+export default function UsersDataProvider() {
+  const [usersData, setUsersData] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  const fetchUsersData = useCallback(() => {
+    const url = import.meta.env.VITE_URL_BASE + "users/all";
+    const options = {
+      mode: "cors",
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      credentials: "include",
+    };
+
+    fetch(url, options)
+      .then(r => {
+        if (r.status >= 400) {
+          throw new Error("Server error");
+        }
+
+        return r.json();
+      })
+      .then(data => setUsersData(data))
+      .catch(e => {
+        console.log("Error fetching all property users data", e), setError(e);
+      })
+      .finally(setIsLoading(false));
+  }, []);
+
+  useEffect(() => {
+    fetchUsersData();
+  }, [fetchUsersData]);
+
+  return {
+    usersData,
+    isLoading,
+    error,
+    refreshUsersData: fetchUsersData,
+  };
+}
