33fae00494d78a58d5f54c08ae45aa419e739feb
tomason182 <elotro182@hotmail.com>
2025-02-18 10:23:21 -0300

    add: payment methods in payment form

--- src/forms/AdvancePaymentForm.jsx
+++ src/forms/AdvancePaymentForm.jsx
@@ -13,27 +13,60 @@ export default function AdvancePaymentForm({ closeModal }) {
 
   return (
     <form className={styles.form}>
-      <div className={styles.formGroup}>
-        <label>
-          Do you required advance payment for a reservation?
-          <select
-            name="advance_payment"
-            onChange={() => setRequiredDeposit(!requiredDeposit)}
-            value={requiredDeposit}
-          >
-            <option value={true}>Yes</option>
-            <option value={false}>No</option>
-          </select>
-        </label>
-      </div>
-      <div className={styles.formGroup}>
-        <label>
-          Amount in percent of the deposit?
-          <select name="amount" disabled={!requiredDeposit}>
-            {amount}
-          </select>
-        </label>
-      </div>
+      <fieldset>
+        <legend>Payment method accepted in the property</legend>
+        <div className={styles.groupContainer}>
+          <div className={styles.checkbox}>
+            <label>
+              Cash
+              <input type="checkbox" name="cash" />
+            </label>
+          </div>
+          <div className={styles.checkbox}>
+            <label>
+              Debit card
+              <input type="checkbox" name="debit" />
+            </label>
+          </div>
+          <div className={styles.checkbox}>
+            <label>
+              Credit card
+              <input type="checkbox" name="credit" />
+            </label>
+          </div>
+          <div className={styles.checkbox}>
+            <label>
+              Bank transfer
+              <input type="checkbox" name="transfer" />
+            </label>
+          </div>
+        </div>
+      </fieldset>
+      <fieldset>
+        <legend>Advance payment Policies</legend>
+        <div className={styles.formGroup}>
+          <label>
+            Do you required advance payment for a reservation?
+            <select
+              name="advance_payment"
+              onChange={() => setRequiredDeposit(!requiredDeposit)}
+              value={requiredDeposit}
+            >
+              <option value={true}>Yes</option>
+              <option value={false}>No</option>
+            </select>
+          </label>
+        </div>
+        <div className={styles.formGroup}>
+          <label>
+            Amount in percent of the deposit?
+            <select name="amount" disabled={!requiredDeposit}>
+              {amount}
+            </select>
+          </label>
+        </div>
+      </fieldset>
+
       <div className={styles.buttonGroup}>
         <button className={styles.cancelButton} onClick={closeModal}>
           Cancel
