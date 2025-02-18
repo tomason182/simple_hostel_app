d9f5ee998c601fb0c302b51462c1ce63c9cca534
tomason182 <elotro182@hotmail.com>
2025-02-18 16:18:34 -0300

    add: Property data provider

---
+++ src/data_providers/PropertyDataProvider.jsx
@@ -0,0 +1,45 @@
+import { useState, useCallback, useEffect } from "react";
+
+export default function PropertyDataProvider() {
+  const [propertyData, setPropertyData] = useState({});
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  const fetchPropertyData = useCallback(() => {
+    const url = import.meta.env.VITE_URL_BASE + "property";
+    const options = {
+      mode: "cors",
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      credentials: "include",
+    };
+
+    fetch(url, options)
+      .then(r => {
+        if (r.status >= 400) {
+          throw new Error("Server Error");
+        }
+
+        return r.json();
+      })
+      .then(data => setPropertyData(data))
+      .catch(e => {
+        console.log("Error fetching property data", e);
+        setError(e);
+      })
+      .finally(setIsLoading(false));
+  });
+
+  useEffect(() => {
+    fetchPropertyData();
+  }, [fetchPropertyData]);
+
+  return {
+    propertyData,
+    isLoading,
+    error,
+    refreshPropertyData: fetchPropertyData,
+  };
+}
