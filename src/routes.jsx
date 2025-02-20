e864720c19ea040d266323166fe76f142c0abef1
tomason182 <elotro182@hotmail.com>
2025-02-20 17:23:11 -0300

    add: create routes

---
+++ src/routes.jsx
@@ -0,0 +1,49 @@
+import Layout from "./Layout";
+import Home from "./pages/Home/Home";
+import Calendar from "./pages/Calendar/Calendar";
+import RatesAndAvailability from "./pages/RatesAndAvailability/RatesAndAvailability";
+import Reservations from "./pages/Reservations/Reservations";
+import Property from "./pages/Property/Property";
+
+const routes = [
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      {
+        path: "calendar",
+        element: <Calendar />,
+      },
+      {
+        path: "rates-and-availability",
+        element: <RatesAndAvailability />,
+      },
+      {
+        path: "reservations",
+        element: <Reservations />,
+      },
+      {
+        path: "property",
+        element: <Property />,
+      },
+      {
+        path: "inbox",
+        element: <h1>Inbox Page</h1>,
+      },
+      {
+        path: "my-website",
+        element: <h1>My Website Page</h1>,
+      },
+      {
+        path: "reports",
+        element: <h1>Reports Page</h1>,
+      },
+    ],
+  },
+];
+
+export default routes;
