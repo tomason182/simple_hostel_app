5ee46ffa62db330dffff0f8b936b19e218f1053a
tomason182 <elotro182@hotmail.com>
2025-04-02 20:22:03 -0300

    translate: children policies form

--- src/forms/ChildrenPoliciesForm.jsx
+++ src/forms/ChildrenPoliciesForm.jsx
@@ -1,6 +1,7 @@
 import styles from "./defaultFormStyle.module.css";
 import { useState } from "react";
 import PropTypes from "prop-types";
+import { useTranslation } from "react-i18next";
 
 export default function ChildrenPoliciesForm({
   closeModal,
@@ -17,7 +18,7 @@ export default function ChildrenPoliciesForm({
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
 
-  console.log(formData);
+  const { t } = useTranslation();
 
   function handleInputChange(e) {
     const { name, value } = e.target;
@@ -62,21 +63,21 @@ export default function ChildrenPoliciesForm({
     <form className={styles.form} onSubmit={handleSubmit}>
       <div className={styles.formGroup}>
         <label className={styles.labelFlex}>
-          Are children allowed in your property?
+          {t("are_children_allowed")}
           <select
             name="allow_children"
             value={formData.allow_children}
             onChange={e => handleInputChange(e)}
             className={styles.inputSmall}
           >
-            <option value="true">Yes</option>
+            <option value="true">{t("yes")}</option>
             <option value="false">No</option>
           </select>
         </label>
       </div>
       <div className={styles.formGroup}>
         <label className={styles.labelFlex}>
-          Children minimum age allowed:
+          {t("minimum_age_allowed")}:
           <input
             type="number"
             className={styles.inputSmall}
@@ -88,7 +89,7 @@ export default function ChildrenPoliciesForm({
       </div>
       <div className={styles.formGroup}>
         <label className={styles.labelFlex}>
-          Children allowed room types:
+          {t("room_types_allowed_for_children")}:
           <select
             type="number"
             className={styles.inputMedium}
@@ -96,15 +97,15 @@ export default function ChildrenPoliciesForm({
             value={formData.minors_room_types}
             onChange={e => handleInputChange(e)}
           >
-            <option value="all_rooms">All room types</option>
-            <option value="only_private">Only Private Rooms</option>
-            <option value="only_dorm">Only dormitories</option>
+            <option value="all_rooms">{t("all")}</option>
+            <option value="only_private">{t("private_rooms")}</option>
+            <option value="only_dorm">{t("dormitories")}</option>
           </select>
         </label>
       </div>
       <div className={styles.formGroup}>
         <label className={styles.labelFlex}>
-          Free stay age (leave 0 for no):
+          {t("free_stay_age")}:
           <input
             className={styles.inputSmall}
             type="number"
@@ -116,10 +117,10 @@ export default function ChildrenPoliciesForm({
       </div>
       <div className={styles.buttonGroup}>
         <button className={styles.cancelButton} onClick={closeModal}>
-          Cancel
+          {t("cancel")}
         </button>
         <button className={styles.submitButton} disabled={loading}>
-          Submit
+          {t("save")}
         </button>
       </div>
       {error && <p className={styles.error}>{error}</p>}
