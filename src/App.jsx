ae573a198aab1a6387dc432e949e2a50dfcbc01b
tomason182 <elotro182@hotmail.com>
2025-04-19 14:49:03 -0300

    fix: move dataProviders to Layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,12 @@
 import "./App.css";
 import { createBrowserRouter, RouterProvider } from "react-router";
 
-// Import Data providers
-import RoomTypeDataProvider from "./data_providers/RoomTypesDataProvider";
-import UserProfileProvider from "./data_providers/UserProfileProvider";
 import routes from "./routes";
 
 function App() {
   const router = createBrowserRouter(routes);
 
-  return (
-    <UserProfileProvider>
-      <RoomTypeDataProvider>
-        <RouterProvider router={router} />
-      </RoomTypeDataProvider>
-    </UserProfileProvider>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
--- src/Layout.jsx
+++ src/Layout.jsx
@@ -2,16 +2,23 @@ import { Outlet } from "react-router";
 import Header from "./components/Header/Header";
 import NavBar from "./components/NavBar/NavBar";
 import Footer from "./components/Footer/Footer";
+// Import Data providers
+import RoomTypeDataProvider from "./data_providers/RoomTypesDataProvider";
+import UserProfileProvider from "./data_providers/UserProfileProvider";
 
 export default function Layout() {
   return (
     <>
-      <Header />
-      <NavBar />
-      <main>
-        <Outlet />
-      </main>
-      <Footer />
+      <UserProfileProvider>
+        <RoomTypeDataProvider>
+          <Header />
+          <NavBar />
+          <main>
+            <Outlet />
+          </main>
+          <Footer />
+        </RoomTypeDataProvider>
+      </UserProfileProvider>
     </>
   );
 }
