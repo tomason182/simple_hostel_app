2b0076d16d9bc28106a054641c1c02dbae7d0825
tomason182 <elotro182@hotmail.com>
2025-04-22 09:51:39 -0300

    add: ToastProvider to app

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import "./App.css";
 import { createBrowserRouter, RouterProvider } from "react-router";
+import ToastProvider from "./components/Toast/ToastProvider";
 
 import routes from "./routes";
 
 function App() {
   const router = createBrowserRouter(routes);
 
-  return <RouterProvider router={router} />;
+  return (
+    <ToastProvider>
+      <RouterProvider router={router} />
+    </ToastProvider>
+  );
 }
 
 export default App;
