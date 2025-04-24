53ebb1bea70c30f4928a9f0fa7d58c7eae605999
tomason182 <elotro182@hotmail.com>
2025-04-24 11:36:18 -0300

    add: new user invitation page

--- src/forms/SetPasswordForm.jsx
+++ src/forms/SetPasswordForm.jsx
@@ -1,31 +1,130 @@
 import styles from "./defaultFormStyle.module.css";
-import { useState } from "react";
+import PropTypes from "prop-types";
 import { useTranslation } from "react-i18next";
+import { useState } from "react";
+import { useToast } from "../hooks/useToast.jsx";
+import passwordValidator from "../utils/passwordValidator.js";
+import { useNavigate } from "react-router";
 
 export default function SetPasswordForm({ token }) {
+  const [error, setError] = useState(null);
+  const [passwordRequirement, setPasswordRequirement] = useState(null);
+  const [loading, setLoading] = useState(false);
   const { t } = useTranslation();
+
+  const { addToast } = useToast();
+  const navigate = useNavigate();
+
+  function handleChange(e) {
+    const value = e.target.value;
+
+    const isValid = passwordValidator(value);
+
+    if (isValid === false) {
+      setPasswordRequirement("PASSWORD_REQUIREMENT");
+    } else {
+      setPasswordRequirement(null);
+    }
+  }
+
+  async function handleSubmit(e) {
+    try {
+      e.preventDefault();
+      setLoading(true);
+      setError(null);
+
+      const fromBody = {
+        newPassword: e.target.password.value,
+        repeatNewPassword: e.target.confirmPassword.value,
+      };
+      const url =
+        import.meta.env.VITE_URL_BASE + "/users/validate/new-user/" + token;
+      const options = {
+        mode: "cors",
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(fromBody),
+      };
+
+      if (fromBody.newPassword !== fromBody.repeatNewPassword) {
+        setError("PASSWORD_NOT_MATCH");
+        return;
+      }
+
+      const isValidPassword = passwordValidator(fromBody.newPassword);
+
+      if (isValidPassword === false) {
+        setError("PASSWORD_REQUIREMENT");
+        return;
+      }
+      const response = await fetch(url, options);
+
+      if (!response.ok) {
+        const error = await response.json();
+        setError(error.msg || "UNEXPECTED_ERROR");
+        return;
+      }
+
+      addToast({
+        message: t("USER_CREATED", { ns: "validation" }),
+        type: "success",
+      });
+
+      navigate("/accounts/auth");
+    } catch (err) {
+      setError(err.message || "UNEXPECTED_ERROR");
+    } finally {
+      setLoading(false);
+    }
+  }
   return (
-    <form>
+    <form
+      className={styles.form}
+      onSubmit={handleSubmit}
+      style={{ maxWidth: "400px" }}
+    >
       <div className={styles.formGroup}>
         <label>
           {t("password")}
-          <input type="password" name="password" required aria-required />
+          <input
+            type="password"
+            name="password"
+            required
+            aria-required
+            onChange={handleChange}
+          />
         </label>
+        {passwordRequirement && (
+          <span className={styles.passwordRequirementMessage}>
+            {t(passwordRequirement, { ns: "validation" })}
+          </span>
+        )}
       </div>
       <div className={styles.formGroup}>
         <label>
           {t("confirm_password")}
           <input
             type="password"
-            name="confirm_password"
+            name="confirmPassword"
             required
             aria-required
           />
         </label>
       </div>
       <div className={styles.buttonContainer}>
-        <button className={styles.submitButton}>{t("submit")}</button>
+        <button className={styles.submitButton} disabled={loading}>
+          {loading ? "Loading..." : t("submit")}
+        </button>
       </div>
+      {error && (
+        <p className={styles.error}>{t(error, { ns: "validation" })}</p>
+      )}
     </form>
   );
 }
+
+SetPasswordForm.propTypes = {
+  token: PropTypes.string.isRequired,
+};
--- src/pages/EmailVerified/NewUser.jsx
+++ src/pages/EmailVerified/NewUser.jsx
@@ -1,34 +1,20 @@
 import SetPasswordForm from "../../forms/SetPasswordForm";
 import styles from "./EmailVerified.module.css";
-import { useState } from "react";
 import { useParams } from "react-router";
 import { jwtDecode } from "jwt-decode";
-import { useNavigate } from "react-router";
+import { useTranslation } from "react-i18next";
+
 export default function NewUser() {
-  const [formData, setFromData] = useState({
-    password: "",
-    confirmPassword: "",
-    token: "",
-  });
-  const [error, setError] = useState(null);
-  const [loading, setLoading] = useState(false);
   const { token } = useParams();
   const propertyName = jwtDecode(token).propertyName;
-
-  function handleFormSubmit(e) {
-    e.preventDefault();
-
-    const url = import.meta.env.VITE_URL_BASE;
-  }
+  const { t } = useTranslation();
 
   return (
     <div className={styles.mainContent}>
-      <h1>Welcome to Simple Hostel</h1>
-      <h3>{propertyName} has invite you to join their team</h3>
-      <p className={styles.text}>
-        To get started, please create a password for your account.
-      </p>
-      <SetPasswordForm />
+      <h1>{t("welcome_simple_hostel")}</h1>
+      <h3>{t("invitation", { name: propertyName })}</h3>
+      <p className={styles.text}>{t("get_started")}</p>
+      <SetPasswordForm token={token} />
     </div>
   );
 }
